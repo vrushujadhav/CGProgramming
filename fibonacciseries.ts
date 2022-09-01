4b920622c89e732574fe886f4e59f8d92a4be521
DESKTOP-E0T9KGH\Rakshas <vrushalijadhav956@gmail.com>
2022-08-31 23:28:38 -0700

    It is a code for fibonacci series

---
+++ fibonacciseries.ts
@@ -0,0 +1,14 @@
+let n:number=10;
+let n1:number=1;
+console.log(n1);
+let n2:number=1;
+console.log(n2);
+let n3:number;
+let i:number;
+for (i=3;i<=10;i++){
+    n3=n1+n2;
+    console.log(n3);
+    n2=n1;
+    n3=n2;
+}
+
